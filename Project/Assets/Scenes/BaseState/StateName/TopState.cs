d8bca95ecdecfa2b530e3d78ae3d7433b96ed502
M-Yuki-Hasuo <huyuda1220@gmail.com>
2022-09-17 14:31:24 +0900

    stateコンポーネントの追加

--- Project/Assets/Scenes/BaseState/StateName/TopState.cs
+++ Project/Assets/Scenes/BaseState/StateName/TopState.cs
@@ -12,6 +12,8 @@
     public void OnEnter()
     {
         this.topStateComponent.Init(() => this.stateMachine.ChangeState(StateName.MAP));
+        this.topStateComponent.OnEnter();
+
     }
 
     public void OnUpdate()
